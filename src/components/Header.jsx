9fe85d6bd88c74251bb870a721a3025d835737d8
Rachit <rachitsingh72@gmail.com>
2025-11-26 15:33:38 +0530

    initial project setup

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header(){
+  return (
+    <header className="Header">
+      <img src="chef-claude-icon.png" alt="chef claude" />
+      <h1>Chef Claude</h1>
+    </header>
+  )
+}
